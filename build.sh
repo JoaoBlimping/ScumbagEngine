69c65744df7b46c835a76536228a2b59b0bda571
marineorganism <marineorganism@gmail.com>
2017-07-09 22:51:44 +1000

    Added this TMX thingo and a license for it's sake

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # $3 -> flags
 
 CC=gcc
-FILES='main.c Image.c ini.c LinkedList.c load.c render.c Tileset.c'
+FILES='main.c Image.c ini.c LinkedList.c load.c render.c Tileset.c tmx/tmx_err.c tmx/tmx_utils.c tmx/tmx_xmlc tmx/tmx_tmx.c'
 FLAGS='-lSDL2 -lSDL2_image'
 DIR=`pwd`
 
